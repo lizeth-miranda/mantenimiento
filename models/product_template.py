b03b7b9ff042b1ad83ccd856708c18c7343cea4d
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-11-10 16:58:18 -0700

    Update product_template.py

--- models/product_template.py
+++ models/product_template.py
@@ -6,21 +6,6 @@ from odoo import api, fields, models, _
 class promaint(models.Model):
     _inherit = 'product.template'
 
-    # enviar = fields.Selection([
-    #     ('crear', 'Crear en Mantenimiento'), ], string="Enviar",)
-
-    # @api.model
-    # def create(self, vals_list):
-    #     res = super(promaint, self).create(vals_list)
-    #     if self.enviar in ['crear', 'Crear en Mantenimiento']:
-    #         print("funciona")
-    #         vals = {
-    #             'name': res.name,
-    #             'serial_no': res.num_serie,
-    #         }
-    #         self.env['maintenance.equipment'].create(vals)
-
-    #     return res
     marca = fields.Char(string="Marca",)
     modelo = fields.Char(string="Modelo",)
     proveedor = fields.Many2one(

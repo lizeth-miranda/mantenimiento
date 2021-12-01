82c186bb56a67148814634fb0a6ec697a98ddd31
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-12-01 13:22:31 -0700

    Update product_template.py

--- models/product_template.py
+++ models/product_template.py
@@ -13,19 +13,27 @@ class promaint(models.Model):
 
     def enviar(self):
         for record in self:
-            vals = {
-                'name': record.name,
-                'serial_no': record.num_serie,
-                'model': record.modelo,
-                'marca': record.marca,
-                'partner_id': record.proveedor.id,
-                'cost': record.standard_price,
-            }
-            record.env['maintenance.equipment'].create(vals)
-            return {
-                'effect': {
-                    'fadeout': 'slow',
-                    'message': 'Registro Exitoso',
-                    'type': 'rainbow_man',
+            buscar = self.env['maintenance.equipment'].search_count([
+                ('serial_no', '=', record.num_serie),
+            ])
+            if buscar > 0:
+                raise ValidationError(
+                    _("Uno o varios de los registros ya existen"))
+
+            elif not buscar:
+                vals = {
+                    'name': record.name,
+                    'serial_no': record.num_serie,
+                    'model': record.modelo,
+                    'marca': record.marca,
+                    'partner_id': record.proveedor.id,
+                    'cost': record.standard_price,
                 }
-            }
+                record.env['maintenance.equipment'].create(vals)
+                return {
+                    'effect': {
+                        'fadeout': 'slow',
+                        'message': 'Registro Exitoso',
+                        'type': 'rainbow_man',
+                    }
+                }

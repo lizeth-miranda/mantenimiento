e9cb3d51edbc28b164bc47c5bffb77a61462cf9d
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2022-11-15 16:01:45 -0600

    Update maintenance_equip.py

--- models/maintenance_equip.py
+++ models/maintenance_equip.py
@@ -10,3 +10,17 @@ class mainten(models.Model):
     obra = fields.Many2one(
         comodel_name='account.analytic.account', string="Obra", )
     sistemas = fields.Boolean(string='Sistemas', default=False,)
+    
+    rev = fields.Char(string="Revisión",)
+    fecha_publ = fields.Date(string="Fecha de Publicación",)
+    fecha_prox = fields.Date(string="Fecha de Próxima Revisión",)
+    cod = fields.Char(string="Código",)
+    nota = fields.Char(string="Nota",)
+
+    employee = fields.Many2one('hr.employee', string='Empleado', tracking=True)
+    fecha = fields.Date(string="Fecha")
+    department = fields.Char(
+        related="employee.department_id.name",
+        string="Departamento",
+    )
+    puesto = fields.Char(related="employee.job_id.name", string="Puesto")
